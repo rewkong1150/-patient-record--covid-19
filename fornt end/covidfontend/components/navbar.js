d5ad65e5b668f96bd4b98b666b2b1b8adbccc68d
rewkong1150 <70642159+rewkong1150@users.noreply.github.com>
2022-04-27 21:11:45 +0700

    Update navbar.js

--- fornt end/covidfontend/components/navbar.js
+++ fornt end/covidfontend/components/navbar.js
@@ -1,13 +1,23 @@
 import Link from 'next/link'
-
 const Navbar = () => (
-    <div>
-        <Link href="/"><a> Home </a></Link> |
-        <Link href="/profile"><a> Profile </a></Link> | 
-        <Link href="/patient"><a>ข้อมูลผู้ป่วย</a></Link> |
-        <Link href="/foo"><a> Foo </a></Link> |
-        <Link href="/login"><a> Login </a></Link> |
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+         <div className='mr-4 bg-indigo-500 p-2 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-lime-500'>
+         <Link href="/Home"><a> Home </a></Link> |
+        
+        <Link href="/patient"><a>Patient</a></Link> |
+        
+        
+        <Link href="/addpatient"><a>addpatient</a></Link> |
+        
+        <Link href="/"><a>Login</a></Link> |
+        <Link href="/logout"><a> Logout </a></Link> 
+        
+        </div>
+        </ul>
     </div>
+    </nav>
 )
 
 export default Navbar
